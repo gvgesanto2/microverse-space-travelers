f2519eb446d4ed3dc94d9c524ee928a1865c6d1c
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-07-22 09:34:16 -0600

    Finish main test for my profile page

--- src/components/rockets/test/RocketProfile.test.js
+++ src/components/rockets/test/RocketProfile.test.js
@@ -3,19 +3,19 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import { store } from '../../../redux/store';
-import RocketProfile from '../rocket-profile/RocketProfile.component';
+import MyProfile from '../../../pages/my-profile/my-profile.component';
 
 const renderRocketProfile = () => {
   const container = render(
     <Provider store={store}>
-      <RocketProfile />
+      <MyProfile />
     </Provider>,
   );
   return container;
 };
 
-describe('Rocket Profile', () => {
-  it('Renders the rocket profile component correctly', () => {
+describe('My Profile page', () => {
+  it('Renders my profile component correctly', () => {
     expect(renderRocketProfile()).toMatchSnapshot();
   });
 });
